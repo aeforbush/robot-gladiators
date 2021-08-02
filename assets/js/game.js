3046ebd921607ae316919f8e7f4b94f61bb7d04c
aeforbush <87487655+aeforbush@users.noreply.github.com>
2021-08-02 13:03:24 -0600

    added variable

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,6 @@
-window.alert("This is an alert! JavaScript is running!");
+var playerName = window.prompt("What is your robot's name?"); // Note the lack of quotation marks around playerName window.alert("");
+function fight() {
+    window.alert("The fight has begun!")
+}
+// fight();
+
