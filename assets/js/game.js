199c253b859ed07dd3e46f003218639ffd2989f9
aeforbush <87487655+aeforbush@users.noreply.github.com>
2021-08-02 13:08:31 -0600

    added name alert

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,4 +1,5 @@
-var playerName = window.prompt("What is your robot's name?"); // Note the lack of quotation marks around playerName window.alert("");
+var playerName = window.prompt("What is your robot's name?"); // Note the lack of quotation marks around playerName 
+window.alert(playerName);
 function fight() {
     window.alert("The fight has begun!")
 }
