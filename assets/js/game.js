13e7183dbce5ad810a9755d3003de0de6b2c0906
aeforbush <87487655+aeforbush@users.noreply.github.com>
2021-08-03 17:07:05 -0600

    added round 1 and game over

--- assets/js/game.js
+++ assets/js/game.js
@@ -71,16 +71,15 @@ var fight = function(enemyName) {
 for(var i = 0; i < enemyNames.length; i++) {
     if (playerHealth > 0) {
         window.alert("Welcome to Robot Gladiators! Round " + ( i + 1 ) );
+        var pickedEnemyName = enemyNames[i];
+        enemyHealth = 50;
+        // use debugger to pause script from running and checking what's going on at that moment in the code
+        // debugger;
+    fight(pickedEnemyName);
     }  else {
         window.alert("You have lost your robot in battle! Game Over!");
-        }
-
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
-    // use debugger to pause script from running and checking what's going on at that moment in the code
-    // debugger;
-    fight(pickedEnemyName);
-
+        break;
+    }
 }
 
 //fight();
